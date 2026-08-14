d61ee1eecf4156644dbba9b5f18abc7ff9362331
luisjuniorawr1 <luis.junior.lima@gmail.com>
2026-08-14 14:22:15 -0300

    Add color management interface

---
+++ src/color-management.js
@@ -0,0 +1,6 @@
+export const colorManagement = {
+  mode: 'RGB',
+  cmykAvailable: false,
+  description: 'O arquivo está em RGB. Conversão CMYK certificada requer um pipeline de gerenciamento de cores com perfil ICC.',
+  supportedFutureProfiles: ['FOGRA', 'GRACoL', 'ISO Coated', 'Perfil fornecido pela gráfica'],
+};
